22cf6efa3fef1be39a1ea5e0fe6264a1e06cda57
R.Mizuhara <ham_katsu@outlook.jp>
2017-01-24 21:36:38 +0900

    combine

--- modules/ALU.vhd
+++ modules/ALU.vhd
@@ -1,6 +1,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+
+--library ieee_proposed;
+--use ieee_proposed.fixed_float_types.all;
+--use ieee_proposed.fixed_pkg.all;
+use ieee.fixed_float_types.all;
+use ieee.fixed_pkg.all;
 
 entity ALU is 
     port(A_IN : in std_logic_vector(15 downto 0);
@@ -15,64 +23,52 @@ entity ALU is
 end ALU;
 
 architecture RTL of ALU is 
-component MPU
-    port(A_in: in std_logic_vector(15 downto 0);
-         OP: in std_logic_vector(2 downto 0);
-         B_in: in std_logic_vector(15 downto 0);
-         C_OUT: out std_logic_vector(15 downto 0)
-);
-end component;
-
-signal MPU_A: std_logic_vector(15 downto 0) := X"0000";
-signal MPU_OP: std_logic_vector(2 downto 0);
-signal MPU_B: std_logic_vector(15 downto 0):= X"0000";
-signal MPU_z: std_logic;
-signal out_MPU :std_logic_vector(15 downto 0):= X"0000";
 
 
-
-
-
+signal f_A,f_B: ufixed(7 downto -8);
 
 begin
 
-    COMP: MPU port map(A_in,OP ,B_in,out_MPU);
+    --COMP: MPU port map(A_in,OP ,B_in,out_MPU);
     
 
 	
-    process(A_IN,B_IN,latch,OP)
+    process(A_IN,B_IN,latch,OP,f_A,f_B)
 	variable ans : std_logic_vector(15 downto 0);
     begin
-    --if(CLK'event and CLK = '1') then
+	f_A <= to_ufixed(A_IN, f_A);
+        f_B <= to_ufixed(B_IN, f_B);
 	if (latch = '1') then
           
 	    case OP is
 		when "001" =>
-		    ans := A_IN + B_IN;
+		    BUS_C_OUT <= A_IN + B_IN;
 		when "010" =>
 		    ans := A_IN - B_IN;
 		when "101" => 
-		    ans := A_IN;
+		    BUS_C_OUT <= A_IN;
 		when "110" =>
-		    ans := B_IN;
+		    BUS_C_OUT <= B_IN;
 		when "111" =>
 		    ans := A_IN - B_IN;
 		    if (ans = "0000000000000000") then
 			   zero <= '1';
 		    else
-	    			zero <= '0';
+	    		   zero <= '0';
     		    end if;
 		when "000" =>
-		    ans := X"0000";
-	        when others =>
-		    ans := out_MPU;
+		    BUS_C_OUT <= X"0000";
+	        when "011" =>
+		    BUS_C_OUT <= to_SLV(resize(f_A*f_B,7,-8));
+		when "100" =>
+		    BUS_C_OUT <= to_SLV(resize(f_A/f_B,7,-8));	
+		when others =>
+			null;	    
         end case;
-	BUS_C_OUT <= ans;
-
-	
 	
 	end if;
-    --end if;
+
+	
     end process;
 
  
